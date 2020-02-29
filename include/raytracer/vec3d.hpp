6c0c6302f6707a5d4b9a9b5341fc6cae5d5d40b5
Argha Sen <iamslowdeath@gmail.com>
2020-02-29 17:55:39 +0800

    more vector functionality

--- include/raytracer/vec3d.hpp
+++ include/raytracer/vec3d.hpp
@@ -1,53 +1,189 @@
-#include<array>
-#include<cmath>
+#include <array>
+#include <cmath>
 
 namespace raytracer
 {
 namespace utils
 {
-    template <typename T>
-    class Vec3d
-    {
-    private:
-        std::array<T,3>  m_data{0,0,0};
+
+/*! \brief A 3d vector class.
+ *         Provides basic functionality for creating and manipulating 3d vectors.
+ *
+ */
+
+template <typename T>
+class Vec3d
+{
+private:
+    std::array<T, 3> m_data{0, 0, 0};
+
+public:
+    /// Default Constructor creates 0 initalized 3d vector.
+    Vec3d() = default;
     
-    public:
-        Vec3d() = default;
-        Vec3d(T x, T y, T z);
-        inline T x() const { return m_data[0]; }
-        inline T y() const { return m_data[1]; }
-        inline T z() const { return m_data[2]; }
-        inline double length() const;
-        Vec3d operator+(const Vec3d& u);
-        Vec3d operator-(const Vec3d& u);
-        Vec3d operator*(const Vec3d& u);
-        Vec3d operator/(const Vec3d& u);
-        Vec3d operator.(const Vec3d& u);
-    };
+    /**
+     * @brief Construct a new Vec 3d object
+     * 
+     * @param x  the x coordinate
+     * @param y  the y coordinate
+     * @param z  the z coordinate
+     */
+    Vec3d(T x, T y, T z);
 
-    template <typename T>
-    Vec3d<T>::Vec3d(T x, T y, T z)
-    {
-        m_data[0] = x;
-        m_data[1] = y;
-        m_data[2] = z;
-    }
+    /**
+     * @brief return the x coordinate.
+     * 
+     * @return T 
+     */
+    inline T x() const { return m_data[0]; }
 
-    template <typename T>
-    double Vec3d<T>::length() const
-    {
-        return sqrt(this->x()*this->x() + this->y()*this->y()
-               + this->z()*this->z());
-    }
+    /**
+     * @brief return the y coordinate.
+     * 
+     * @return T 
+     */
+    inline T y() const { return m_data[1]; }
 
-    template <typename T>
-    Vec3d<T> Vec3d<T>::operator+(const Vec3d& u)
-    {
-        Vec3d<T> res;
-        res.m_data[0] = m_data[0]+u.x();
-        res.m_data[1] = m_data[1]+u.y();
-        res.m_data[2] = m_data[2]+u.z();
-        return res;
-    }
+    /**
+     * @brief return the z coordinate.
+     * 
+     * @return T 
+     */
+    inline T z() const { return m_data[2]; }
+
+    /**
+     * @brief length of the vector.
+     * 
+     * @return double 
+     */
+    inline double length() const;
+
+    /**
+     * @brief add two vectors.
+     * 
+     * @param u  a vector
+     * @return Vec3d a new vector with the result of the addition of the 2 vectors.  
+     */
+    Vec3d operator+(const Vec3d &u) const;
+
+    /**
+     * @brief subtract two vectors.
+     * 
+     * @param u a vector.
+     * @return Vec3d a new vector with the result of the subtraction of the 2 vectors.
+     */
+    Vec3d operator-(const Vec3d &u) const;
+
+    /**
+     * @brief multiplication of two vectors
+     * 
+     * @param u a vector.
+     * @return Vec3d a new vector with the result of the multiplication of the 2 vectors.
+     */
+    Vec3d operator*(const Vec3d &u) const;
+
+    /**
+     * @brief division of two vectors
+     * 
+     * @param u a vector.
+     * @return Vec3d a new vector with the result of the division of the 2 vectors.
+     */
+    Vec3d operator/(const Vec3d &u) const;
+
+    /**
+     * @brief scalar multiplication of a vector.
+     * 
+     * @param f a scalar value.
+     * @return Vec3d a new vector with the result of the scalar multiplication of the vector.
+     */
+    template<typename U>
+    Vec3d operator*(U f) const;
+
+    /**
+     * @brief scalar division of a vector.
+     * 
+     * @param f a scalar value.
+     * @return Vec3d a new vector with the result of the scalar division of the vector.
+     */
+    template<typename U>
+    Vec3d operator/(U f) const;
+};
+
+template <typename T>
+Vec3d<T>::Vec3d(T x, T y, T z)
+{
+    m_data[0] = x;
+    m_data[1] = y;
+    m_data[2] = z;
+}
+
+template <typename T>
+double Vec3d<T>::length() const
+{
+    return sqrt(this->x() * this->x() + this->y() * this->y() + this->z() * this->z());
+}
+
+template <typename T>
+Vec3d<T> Vec3d<T>::operator+(const Vec3d &u) const
+{
+    Vec3d<T> res;
+    res.m_data[0] = m_data[0] + u.x();
+    res.m_data[1] = m_data[1] + u.y();
+    res.m_data[2] = m_data[2] + u.z();
+    return res;
+}
+
+template<typename T>
+Vec3d<T> Vec3d<T>::operator-(const Vec3d &u) const
+{
+    Vec3d<T> res;
+    res.m_data[0] = m_data[0] - u.x();
+    res.m_data[1] = m_data[1] - u.y();
+    res.m_data[2] = m_data[2] - u.z();
+    return res;
+}
+
+template<typename T>
+Vec3d<T> Vec3d<T>::operator*(const Vec3d &u) const
+{
+    Vec3d<T> res;
+    res.m_data[0] = m_data[0] * u.x();
+    res.m_data[1] = m_data[1] * u.y();
+    res.m_data[2] = m_data[2] * u.z();
+    return res;
+}
+
+template<typename T>
+Vec3d<T> Vec3d<T>::operator/(const Vec3d &u) const
+{
+    Vec3d<T> res;
+    res.m_data[0] = m_data[0] / u.x();
+    res.m_data[1] = m_data[1] / u.y();
+    res.m_data[2] = m_data[2] / u.z();
+    return res;
+}
+
+template<typename T>
+template<typename U>
+Vec3d<T> Vec3d<T>::operator*(U f) const
+{
+    Vec3d<T> res;
+    res.m_data[0] = m_data[0] * f;
+    res.m_data[1] = m_data[1] * f;
+    res.m_data[2] = m_data[2] * f;
+    return res;
+}
+
+template<typename T>
+template<typename U>
+Vec3d<T> Vec3d<T>::operator/(U f) const
+{
+    Vec3d<T> res;
+    res.m_data[0] = m_data[0] / f;
+    res.m_data[1] = m_data[1] / f;
+    res.m_data[2] = m_data[2] / f;
+    return res;
+}
+
 } // namespace utils
 } // namespace raytracer
--- tests/vec3d_test.cpp
+++ tests/vec3d_test.cpp
@@ -2,7 +2,7 @@
 
 #include <catch2/catch.hpp>
 
-TEST_CASE("3d vectors can be created","[vec3d]")
+TEST_CASE("3d vectors can be created", "[vec3d]")
 {
     raytracer::utils::Vec3d<int> v;
     REQUIRE(v.x() == 0);
@@ -10,33 +10,97 @@ TEST_CASE("3d vectors can be created","[vec3d]")
     REQUIRE(v.z() == 0);
 }
 
-TEST_CASE("Non Zero 3d vectors can be created","[vec3d]")
+TEST_CASE("Non Zero 3d vectors can be created", "[vec3d]")
 {
-    raytracer::utils::Vec3d<int> v(1,2,3);
+    raytracer::utils::Vec3d<int> v(1, 2, 3);
     REQUIRE(v.x() == 1);
     REQUIRE(v.y() == 2);
     REQUIRE(v.z() == 3);
 }
 
-TEST_CASE("Vectors of 0 have 0 length","[vec3d]")
+TEST_CASE("Vectors of 0 have 0 length", "[vec3d]")
 {
     raytracer::utils::Vec3d<int> v;
     REQUIRE(v.length() == 0);
 }
 
-TEST_CASE("Non Zero Vectors have non zero length","[vec3d]")
+TEST_CASE("Non Zero Vectors have non zero length", "[vec3d]")
 {
-    raytracer::utils::Vec3d<int> v(1,1,1);
+    raytracer::utils::Vec3d<int> v(1, 1, 1);
     REQUIRE(v.length() == sqrt(3));
 }
 
-TEST_CASE("Add two vectors","[vec3d]")
+TEST_CASE("Add two vectors", "[vec3d]")
 {
 
-    raytracer::utils::Vec3d<int> v(1,1,1);
-    raytracer::utils::Vec3d<int> u(1,1,1);
+    raytracer::utils::Vec3d<int> v(1, 1, 1);
+    raytracer::utils::Vec3d<int> u(1, 1, 1);
     auto c = u + v;
     REQUIRE(c.x() == 2);
     REQUIRE(c.y() == 2);
     REQUIRE(c.z() == 2);
+}
+
+TEST_CASE("subtract two vectors", "[vec3d]")
+{
+
+    raytracer::utils::Vec3d<int> v(4, 4, 4);
+    raytracer::utils::Vec3d<int> u(1, 1, 1);
+    SECTION("a-b")
+    {
+        auto c = v - u;
+        REQUIRE(c.x() == 3);
+        REQUIRE(c.y() == 3);
+        REQUIRE(c.z() == 3);
+    }
+    SECTION("b-a")
+    {
+        auto c = u - v;
+
+        REQUIRE(c.x() == -3);
+        REQUIRE(c.y() == -3);
+        REQUIRE(c.z() == -3);
+    }
+}
+
+TEST_CASE("Multiply two vectors", "[vec3d]")
+{
+
+    raytracer::utils::Vec3d<int> v(2, 2, 2);
+    raytracer::utils::Vec3d<int> u(3, 3, 3);
+    auto c = u * v;
+    REQUIRE(c.x() == 6);
+    REQUIRE(c.y() == 6);
+    REQUIRE(c.z() == 6);
+}
+TEST_CASE("Divide two vectors", "[vec3d]")
+{
+
+    raytracer::utils::Vec3d<float> v(2, 2, 2);
+    raytracer::utils::Vec3d<float> u(3, 3, 3);
+    auto c = u / v;
+    REQUIRE(c.x() == 1.5);
+    REQUIRE(c.y() == 1.5);
+    REQUIRE(c.z() == 1.5);
+}
+TEST_CASE("Multiply vector with scalar", "[vec3d]")
+{
+
+    raytracer::utils::Vec3d<int> v(2, 2, 2);
+
+    auto c = v*2;
+    REQUIRE(c.x() == 4);
+    REQUIRE(c.y() == 4);
+    REQUIRE(c.z() == 4);
+}
+
+TEST_CASE("Divide vector with scalar", "[vec3d]")
+{
+
+    raytracer::utils::Vec3d<int> v(2, 2, 2);
+
+    auto c =  v/2;
+    REQUIRE(c.x() == 1);
+    REQUIRE(c.y() == 1);
+    REQUIRE(c.z() == 1);
 }
