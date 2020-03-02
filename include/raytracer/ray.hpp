8bee3da7b2713b5c7df0f4f948706c029b5b7aba
Argha Sen <iamslowdeath@gmail.com>
2020-03-02 22:47:45 +0800

    add parameteric point value

--- include/raytracer/ray.hpp
+++ include/raytracer/ray.hpp
@@ -45,6 +45,14 @@ public:
      */
     Vec3d<T> direction() const;
 
+    /**
+     * @brief The point at the parameter value
+     * 
+     * @param t 
+     * @return Vec3d<T> A point in 3d space representing ray at the parameter value.
+     */
+    Vec3d<T> pointAtParameter(T t) const;
+
 private:
     Vec3d<T> m_origin;
     Vec3d<T> m_direction;
@@ -69,5 +77,10 @@ Ray3d<T>::Ray3d(const Vec3d<T> &origin, const Vec3d<T> &direction) : m_origin(or
 {
 }
 
+template <typename T>
+Vec3d<T> Ray3d<T>::pointAtParameter(T t) const
+{
+    return m_origin +  m_direction * t;
+}
 } // namespace utils
 } // namespace raytracer
--- tests/ray_test.cpp
+++ tests/ray_test.cpp
@@ -19,3 +19,19 @@ TEST_CASE("create ray with origin and direction", "[Ray]")
     REQUIRE(tr.origin() == origin);
     REQUIRE(tr.direction() == direction);
 }
+
+TEST_CASE("parametric point","[ray]")
+{
+    auto origin = Vec3d<float>{1,1,1};
+    auto direction = Vec3d<float>{2,2,2};
+    Ray3d<float> tr{origin, direction};
+
+    auto c = tr.pointAtParameter(0);
+    REQUIRE(c == Vec3d<float>{1,1,1});
+
+    auto d = tr.pointAtParameter(1);
+    REQUIRE(d == Vec3d<float>{3,3,3});
+    
+    auto e = tr.pointAtParameter(2);
+    REQUIRE(e == Vec3d<float>{5,5,5});
+}
