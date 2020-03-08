34e2f6dc9965080bbf90c5b8faa5efade6dce8fb
Argha Sen <iamslowdeath@gmail.com>
2020-03-08 10:58:16 +0800

    matrix equality

--- include/raytracer/transforms/details/matrix_impl.hpp
+++ include/raytracer/transforms/details/matrix_impl.hpp
@@ -2,21 +2,33 @@
 
 namespace raytracer::transformations::detail
 {
-template <typename T>
-class Matrix4d
+template <typename T, size_t M = 4, size_t N = 4>
+class Matrix
 {
 private:
     std::array<std::array<T, 4>, 4> m_data;
+    size_t m_M;
+    size_t m_N;
 
 public:
-    Matrix4d() = default;
-
-    Matrix4d(std::initializer_list<std::initializer_list<T>> values)
+    Matrix():
+        m_M(M),m_N(N)
     {
-        int i =0; 
+        m_data = {{
+            {{0, 0, 0, 0}},
+            {{0, 0, 0, 0}},
+            {{0, 0, 0, 0}},
+            {{0, 0, 0, 0}},
+        }};
+    }
+
+    Matrix(std::initializer_list<std::initializer_list<T>> values):
+        m_M(M), m_N(N)
+    {
+        int i = 0;
         for (const auto internalValue : values)
         {
-            int j =0;
+            int j = 0;
             for (const auto val : internalValue)
             {
                 m_data[i][j] = val;
@@ -25,9 +37,34 @@ public:
             i++;
         }
     }
-    T operator()(int i, int j)
+
+    T operator()(int i, int j) const
     {
+        if (i >= m_M || j >= m_N)
+        {
+            throw std::out_of_range("Matrix subscript are out of range");
+        }
         return m_data[i][j];
+    }
+
+    bool operator==(const Matrix<T, M, N> &m) const
+    {
+        for (int i = 0; i < m_M; i++)
+        {
+            for (int j = 0; j < m_N; j++)
+            {
+                if(m_data[i][j] != m(i, j))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool operator!=(const Matrix<T, M, N> &m) const
+    {
+        return !(*this == m);
     }
 };
 
--- tests/matrix_impl_test.cpp
+++ tests/matrix_impl_test.cpp
@@ -2,9 +2,9 @@
 #include <catch2/catch.hpp>
 
 using namespace raytracer::transformations::detail;
-TEST_CASE("4d matrices can be created", "[4dmatrix]")
+TEST_CASE("4d matrices can be created by default", "[4dmatrix]")
 {
-    Matrix4d<float> m;
+    Matrix<float> m;
     REQUIRE(m(0, 0) == Approx(0.0f));
     REQUIRE(m(0, 3) == Approx(0.0f));
     REQUIRE(m(1, 0) == Approx(0.0f));
@@ -14,16 +14,14 @@ TEST_CASE("4d matrices can be created", "[4dmatrix]")
     REQUIRE(m(3, 2) == Approx(0.0f));
 }
 
-
-
-TEST_CASE("4d matrices can be created", "[4dmatrix]")
+TEST_CASE("4d matrices can be created", "matrix]")
 {
-    Matrix4d<float> m = {
-                            {1, 2, 3, 4},
-                            {5.5, 6.5, 7.5, 8.5},
-                            {9, 10, 11, 12},
-                            {13.5, 14.5, 15.5, 16.5}
-                        };
+    Matrix<float> m = {
+        {1, 2, 3, 4},
+        {5.5, 6.5, 7.5, 8.5},
+        {9, 10, 11, 12},
+        {13.5, 14.5, 15.5, 16.5}};
+
     REQUIRE(m(0, 0) == Approx(1.0f));
     REQUIRE(m(0, 3) == Approx(4.0f));
     REQUIRE(m(1, 0) == Approx(5.5f));
@@ -31,4 +29,62 @@ TEST_CASE("4d matrices can be created", "[4dmatrix]")
     REQUIRE(m(2, 2) == Approx(11.0f));
     REQUIRE(m(3, 0) == Approx(13.5f));
     REQUIRE(m(3, 2) == Approx(15.5f));
+}
+
+TEST_CASE("2d matrices are representable", "[matrix]")
+{
+    Matrix<float, 2, 2> m = {
+        {-3, 5},
+        {1, -2}};
+
+    REQUIRE(m(0, 0) == Approx(-3));
+    REQUIRE(m(0, 1) == Approx(5));
+    REQUIRE(m(1, 0) == Approx(1));
+    REQUIRE(m(1, 1) == Approx(-2));
+}
+
+TEST_CASE("3d matrices are representable", "[matrix]")
+{
+    Matrix<float, 3, 3> m = {
+        {-3, 5, 0},
+        {1, -2, -7},
+        {0, 1, 1}};
+
+    REQUIRE(m(0, 0) == Approx(-3));
+    REQUIRE(m(1, 1) == Approx(-2));
+    REQUIRE(m(2, 2) == Approx(1));
+}
+
+TEST_CASE("Matrix equality", "[matrix]")
+{
+    Matrix<float> m = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}};
+
+    Matrix<float> n = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}};
+    
+    REQUIRE(m == n);
+}
+
+TEST_CASE("Matrix inequality", "[matrix]")
+{
+    Matrix<float> m = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}};
+
+    Matrix<float> n = {
+        {11, 12, 13, 14},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}};
+    
+    REQUIRE(m != n);
 }
